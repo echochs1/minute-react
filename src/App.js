dc6c73b5f3014856af726482e3cb6ea657b8fed4
Myka Kugaya <mykakug129@gmail.com>
2022-03-23 16:04:47 -0400

    react navigation installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 // import logo from './logo.svg';
 import './App.css';
-import ButtonRecord from './components/ButtonRecord';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TabBarItem from "./src/components/TabBarItem";
+
+import ForYou from './src/screens/ForYouScreen';
+import Learn from './src/screens/LearnScreen';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <div className="App">
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Tab" options={{ headerShown: false, animationEnabled: false }}>
+            {() => (<Tab.Navigator >
+                <Tab.Screen name="For You" component={ForYou} options={{
+                  TabBarItem: ({ focused }) => (<TabBarItem focused={focused} iconName="record" text="For You"/>),
+                }}/>
+                <Tab.Screen name="Learn" component={Learn} options={{
+                  TabBarItem: ({ focused }) => (<TabBarItem focused={focused} iconName="work" text="Learn"/>),
+                }}/>
+            </Tab.Navigator>)}
+        </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +42,6 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <ButtonRecord/>
     </div>
   );
 }
---
+++ src/screens/ForYouScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ButtonRecord from '../components/ButtonRecord';
+
+export default function ForYouScreen() {
+    return (
+        <ButtonRecord/>
+    )
+}
