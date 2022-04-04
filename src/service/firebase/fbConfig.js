cd774c9d1ec7438e9c3c4bca0ca1e81062580bd0
Myka Kugaya <mykakug129@gmail.com>
2022-04-03 21:44:18 -0400

    firebase auth moving signin out functions

--- src/service/firebase/fbConfig.js
+++ src/service/firebase/fbConfig.js
@@ -25,43 +25,43 @@ export const provider = new GoogleAuthProvider();
 export const db = getDatabase(app);
 // export const analytics = getAnalytics(app);
 
-// export const fbSignIn = async () => {
-//     const isAnon = auth.currentUser.isAnonymous;
-//     provider.setCustomParameters({ prompt: 'select_account' });
-//         signInWithPopup(auth.currentUser, provider)
-//         .then((result) => {
-//             // This gives you a Google Access Token. You can use it to access the Google API.
-//             const credential = GoogleAuthProvider.credentialFromResult(result);
-//             const token = credential.accessToken;
-//             // The signed-in user info.
-//             const user = result.user;
-//             console.log("Logged in with Google: "+user);
-//             // Check if the user was signed in anonymously, if so link the account
-//             if(isAnon) {
-//                 linkWithCredential(auth.currentUser, credential)
-//                 .then((usercred) => {
-//                     const user = usercred.user;
-//                     console.log("Anonymous account successfully upgraded", user);
-//                 }).catch((error) => {
-//                     console.log("Error upgrading anonymous account", error);
-//                 });
-//             }
-//         })
-//         .catch((error) => {
-//             // Handle Errors here.
-//             const errorCode = error.code;
-//             const errorMessage = error.message;
-//             // The email of the user's account used.
-//             const email = error.email;
-//             // The AuthCredential type that was used.
-//             const credential = GoogleAuthProvider.credentialFromError(error);
-//             console.log("Error with Google log-in: "+errorMessage);
-//         });
-// };
+export const fbSignIn = async () => {
+    // const isAnon = auth.currentUser.isAnonymous ? true : false;
+    provider.setCustomParameters({ prompt: 'select_account' });
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log("Logged in with Google: "+user);
+            // Check if the user was signed in anonymously, if so link the account
+            // if(isAnon) {
+            //     linkWithCredential(auth.currentUser, credential)
+            //     .then((usercred) => {
+            //         const user = usercred.user;
+            //         console.log("Anonymous account successfully upgraded", user);
+            //     }).catch((error) => {
+            //         console.log("Error upgrading anonymous account", error);
+            //     });
+            // }
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log("Error with Google log-in: "+errorMessage);
+        });
+};
 
-// export const fbSignOut = () => {
-//     signOut(auth.currentUser).then(() => {
-//         // Sign-out successful.
-//         console.log("logged out");
-//     })
-// }
+export const fbSignOut = () => {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+        console.log("logged out");
+    })
+}
