bcef6eea9e643f121d9df65b9d38270bc1958578
andyyvo <andyvo@bu.edu>
2022-04-03 20:16:05 -0400

    RECORD BUTTON LINKED TO UI but need to configure padding

--- src/components/pages/recording/OneMinPage.js
+++ src/components/pages/recording/OneMinPage.js
@@ -3,6 +3,8 @@ import { Statistic, Slider } from "antd";
 import { randomPrompts } from "../../../service/recording/one-min/randomPrompts";
 import Play from "../../../assets/images/play.svg";
 import Mic from "../../../assets/images/mic.svg";
+import Clock from "../../../assets/images/clock.svg";
+import ButtonRecord from "../main-app/ButtonRecord";
 
 const { Countdown } = Statistic;
 
@@ -18,14 +20,16 @@ const OneMinPage = () => {
             <div className="timer">
                 <Slider min={0} max={60} defaultValue={60} reverse={true} handleStyle={{margin: "1rem"}} />
                 <div className="clock">
+                    <img className="clock-icon" src={Clock} alt="clock" />
                     <Countdown format="mm:ss" valueStyle={{color: "#fff"}} />
                 </div>
             </div>
             <div className="question">
                 <h1 className="question-prompt">{question}</h1>
             </div>
-            <div className="record-btn">
-                <button><img src={Play} alt="Play Button" /></button>
+            <div className="record">
+                {/* <button className="record-btn"><img src={Play} alt="Play Button" /></button> */}
+                <ButtonRecord />
             </div>
         </div>
     )
