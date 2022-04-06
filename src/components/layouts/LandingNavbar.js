6b3a4833bf272eb944ae807a9f31dd6f90c30416
Myka Kugaya <mykakug129@gmail.com>
2022-04-06 16:47:33 -0400

    landing nav avatar styling

--- src/components/layouts/LandingNavbar.js
+++ src/components/layouts/LandingNavbar.js
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import { Button, Avatar, Tooltip, Popover } from "antd";
+import { Button, Avatar, Tooltip, Typography } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import Logo from './logo-blue-teal.svg';
 import { useNavigate } from "react-router-dom";
@@ -34,13 +34,13 @@ const LandingNavbar = () => {
             <div className="nav-links">
                 <Button type="text" onClick={handleAboutClick}><span className="button-text-text text-white">About</span></Button>
                 {authUser.loggedIn ?
-                    <Tooltip title={authUser.displayName} placement="bottom">
-                    {/* <Popover content={<p>{authUser.displayName}</p>} title={authUser.displayName} trigger="hover"> */}
-                    {authUser.photoUrl ?
-                    <Avatar src={authUser.photoUrl} alt={authUser.displayName} size="large" onClick={handleLoginClick}/> 
-                    : <Avatar icon={<UserOutlined />} alt={authUser.displayName} size="large" onClick={handleLoginClick}/>}
-                    </Tooltip>
-                    // </Popover>
+                    <div style={{paddingRight: "1.5em"}}>
+                        <Tooltip title={authUser.displayName} color="white" placement="bottom">
+                        {authUser.photoUrl ?
+                        <Avatar src={authUser.photoUrl} alt={authUser.displayName} size="large" onClick={handleLoginClick}/> 
+                        : <Avatar icon={<UserOutlined />} alt={authUser.displayName} size="large" onClick={handleLoginClick}/>}
+                        </Tooltip>
+                    </div>
                 : <Button type="text" onClick={handleLoginClick}><span className="button-text-text text-white">Login</span></Button>
                 }
             </div>
