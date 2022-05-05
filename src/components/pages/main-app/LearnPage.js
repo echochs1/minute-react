a8c919d842af415f67bccc0db8ced2ade7cdb066
Ashley Kim <ahykim@bu.edu>
2022-05-05 00:14:28 -0400

    changing learn page ui

--- src/components/pages/main-app/LearnPage.js
+++ src/components/pages/main-app/LearnPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { moneyverse } from "../../../assets/images/moneyverse";
+import { logos } from "../../../assets/images/logos/logos";
 import "antd/dist/antd.css";
 // import "./index.css";
 import { List, Avatar, Space } from "antd";
@@ -53,23 +54,25 @@ const Learn = () => {
       id: 6,
       name: "How to Stop Saying “Um,” “Ah,” and “You Know”",
       type: "Article Link",
-      content: "Published August 01, 2018.",
+      content:
+        "From Harvard Business Review: When you get rattled while speaking — whether you’re nervous, distracted, or at a loss for what comes next — it’s easy to lean on filler words, such as “um,” “ah,” or “you know.” These words can become crutches that diminish our credibility and distract from our message. To eliminate such words from your speech, replace them with pauses. To train yourself to do this, take these three steps. First, identify your crutch words and pair them with an action. Every time you catch yourself saying “like,” for example, tap your leg. Once you’ve become aware of your filler words as they try to escape your lips, begin forcing yourself to be silent. Finally, practice more than you think you should. The optimal ratio of preparation to performance is one hour of practice for every minute of presentation. ",
       address: "https://hbr.org/2018/08/how-to-stop-saying-um-ah-and-you-know",
     },
     {
       id: 7,
-      name: "Speaking as a Skill for Success",
-      type: "Article Link",
-      content: "",
-      address:
-        "https://www.edutopia.org/practice/public-speaking-oracy-skills-real-world",
+      name: "The 110 techniques of communication and public speaking",
+      type: "Video Link",
+      content:
+        "From TEDxTALKS: David JP Phillips has spent 7 years studying 5000 speakers, amateurs and professionals in order for the first time in history to detail every single skill a communicator from stage or in a presentation uses in order to deliver their message. This TEDx talk gives you the very most important ones to bring with you to your next presentation or even everyday communication!",
+      address: "https://youtu.be/K0pxo-dS9Hc",
     },
     {
       id: 8,
       name:
         "How The Pandemic Is Giving Public Speakers A Boost: Four Silver Linings To Presenting Virtually",
       type: "Article Link",
-      content: "",
+      content:
+        "From Forbes: ...Force majeure — well, that's an understatement. There is a greater force out there — and yes, due to unforeseen circumstances — that is actually opening doors for people to become better public speakers. Sounds kind of counterintuitive, doesn't it? Conferences and conference attendance aren't what they used to be. There are no microphones, podiums or crowded audiences for speakers. There's no buzz in the hallways and no immediate feedback. But then again, there's also no tripping hazard on the way to the stage.",
       address:
         "https://www.forbes.com/sites/forbesagencycouncil/2020/11/17/how-the-pandemic-is-giving-public-speakers-a-boost-four-silver-linings-to-presenting-virtually/?sh=464527f754f6",
     },
@@ -78,23 +81,25 @@ const Learn = () => {
       name:
         "A Communication Coach's 3 Best Tips to Make Zoom Presentations Less Awkward",
       type: "Article Link",
-      content: "",
+      content:
+        "From Inc. : Some people love public speaking. Others dread it. But absolutely no one gets excited about presenting over Zoom. Why would they? Presenting over video generally means speaking at your computer for however many minutes, anxiously wondering how your speech is going down while worrying whether your neck looks funny from the camera angle you've chosen. That hardly sounds like a blast for you or for the audience. But according to communication coach and Georgetown University professor Sarah Gershman, it doesn't have to be this way. You may never be able to bathe in the buzz of the room over Zoom, but on HBR recently Gershman said that it is possible to make presenting over video way less awkward. Here are her tips. ",
       address:
         "https://www.inc.com/jessica-stillman/communication-public-speaking-zoom-presentation-tips.html",
     },
     {
       id: 10,
-      name: "How to Stop Saying “Um,” “Ah,” and “You Know”",
+      name: "Public Speaking Tips from a Communication Prof",
       type: "Article Link",
-      content: "",
-      address: "https://hbr.org/2018/08/how-to-stop-saying-um-ah-and-you-know",
+      content:
+        "From Cornell University: When it comes to knowing how to talk in front of a crowd—well, Jodi Cohen could give a speech about it. A senior lecturer in the Department of Communication, Cohen has been teaching the art and science of public speaking for more than three decades. Prior to coming to Cornell she was on the faculty at Ithaca College, where she still holds emerita status. On the Hill, her oral communication course—COMM 2010, which is mandatory for majors and satisfies a CALS distribution requirement—attracts more than 300 students each year.",
+      address: "https://alumni.cornell.edu/cornellians/public-speaking-tips/",
     },
     {
       id: 11,
       name: "8 subtle shifts that will help you enjoy public speaking on Zoom",
       type: "Article Link",
       content:
-        "...With a few subtle shifts ranging from mindset tricks to very practical habits, that’s entirely possible. Here are eight expert-approved speaking tips to embrace before your next virtual presentation. Published May 12, 2021.",
+        "From Ladders: Public speaking is one thing. Public speaking on Zoom comes with a whole new dimension of challenges, especially if you get nervous about presenting. From potential technical issues to being unable to rely on non-verbal cues from your audience, it’s easy to break a sweat about all the things that could go wrong while giving a virtual talk. But what if you could not only manage your performance anxiety but also learn how to enjoy public speaking on Zoom calls? With a few subtle shifts ranging from mindset tricks to very practical habits, that’s entirely possible. Here are eight expert-approved speaking tips to embrace before your next virtual presentation. Take some and leave some depending on what feels right for you, as the whole idea is to adopt practices that will help you have fun with the experience (and deliver a better speech in the process).",
       address:
         "https://www.theladders.com/career-advice/8-subtle-shifts-that-will-help-you-enjoy-public-speaking-on-zoom",
     },
@@ -125,12 +130,12 @@ const Learn = () => {
         <List
           itemLayout="vertical"
           size="large"
-          pagination={{
-            onChange: (page) => {
-              console.log(page);
-            },
-            pageSize: 5,
-          }}
+          // pagination={{
+          //   onChange: (page) => {
+          //     console.log(page);
+          //   },
+          //   pageSize: 5,
+          // }}
           dataSource={articles}
           // footer={
           //   <div>
@@ -138,9 +143,9 @@ const Learn = () => {
           //   </div>
           // }
           renderItem={(item) => (
-            <List.Item key={item.id} style={{ fontSize: "15px" }}>
+            <List.Item key={item.id} style={{ fontSize: "13px" }}>
               <List.Item.Meta
-                avatar={<Avatar src={moneyverse[randomIndex(moneyverse)]} />}
+                avatar={<Avatar src={logos[randomIndex(logos)]} />}
                 title={
                   <a
                     href={item.address}
@@ -167,7 +172,6 @@ const Learn = () => {
     </div>
   );
 };
-
 
 // const renderLearnPage = () => {
 //   return (
--- src/components/pages/recording/FinishedPage.js
+++ src/components/pages/recording/FinishedPage.js
@@ -1,82 +1,105 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { fbGetRecording } from "../../../service/firebase/fbConfig";
-import {Space, Spin} from 'antd';
-import { parseDisfluencies, redHighlight } from "../../../service/recording/fillerWordDetect";
+import { Space, Spin } from "antd";
+import {
+  parseDisfluencies,
+  redHighlight,
+} from "../../../service/recording/fillerWordDetect";
 
 const Finished = () => {
-    const location = useLocation();
-    const history = useNavigate();
-    const [recordingData, setRecordingData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
+  const history = useNavigate();
+  const [recordingData, setRecordingData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        // setRecordingData({name: location.state.name, prompt: location.state.prompt, transcript: location.state.transcript, url:fbGetUrl(location.state.name)});
-        setRecordingData(fbGetRecording(location.state.name));
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 5000);   // sleep for 5 seconds before fetching the recoding to ensure url has been uploaded
-    }, []);
+  useEffect(() => {
+    // setRecordingData({name: location.state.name, prompt: location.state.prompt, transcript: location.state.transcript, url:fbGetUrl(location.state.name)});
+    setRecordingData(fbGetRecording(location.state.name));
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000); // sleep for 5 seconds before fetching the recoding to ensure url has been uploaded
+  }, []);
 
-    const handleHomeClick = () => {
-        history("/app");
-    }
+  const handleHomeClick = () => {
+    history("/app/record");
+  };
 
-    const handleRecordClick = () => {
-        history("/one-min");
-    }
+  const handleRecordClick = () => {
+    history("/one-min");
+  };
 
-    const renderResults = () => {
-        console.log(isLoading);
-        if(!isLoading && recordingData) {
-            return (
-                <div className="finishedResults">
-                    <div className="finishedResults-title">
-                        <h1>Results</h1>
-                    </div>
-                    <div className="finishedResults-textWrapper">
-                        <span className="finishedResults-textContent"><b>Topic:</b> {recordingData.prompt}</span>
-                    </div>
-                    <hr className="recordingLine" size="2px" width="100%" color="#BBD2E7"></hr>
-                    <div className="finishedResults-textWrapper">
-                        <span className="finishedResults-textContent"><b>Text:</b> {redHighlight(recordingData.transcript)}</span>
-                    </div>
-                    <hr className="recordingLine" size="2px" width="100%" color="#BBD2E7"></hr>
-                    {recordingData.url && 
-                        <audio controls>
-                            <source src={recordingData.url} type="audio/mpeg" />
-                        </audio>
-                    }
-                    <Space size="large" style={{ marginTop: "20px" }} />
-                    <div className="finishedResults-buttons">
-                        <button className="finishedResults-button-1" onClick={handleRecordClick}>Generate another prompt</button>
-                        <button className="finishedResults-button-2" onClick={handleHomeClick}>Return to app</button>
-                    </div>
+  const renderResults = () => {
+    console.log(isLoading);
+    if (!isLoading && recordingData) {
+      return (
+        <div className="finishedResults">
+          <div className="finishedResults-title">
+            <h1>Results</h1>
+          </div>
+          <div className="finishedResults-textWrapper">
+            <span className="finishedResults-textContent">
+              <b>Topic:</b> {recordingData.prompt}
+            </span>
+          </div>
+          <hr
+            className="recordingLine"
+            size="2px"
+            width="100%"
+            color="#BBD2E7"
+          ></hr>
+          <div className="finishedResults-textWrapper">
+            <span className="finishedResults-textContent">
+              <b>Text:</b> {redHighlight(recordingData.transcript)}
+            </span>
+          </div>
+          <hr
+            className="recordingLine"
+            size="2px"
+            width="100%"
+            color="#BBD2E7"
+          ></hr>
+          {recordingData.url && (
+            <audio controls>
+              <source src={recordingData.url} type="audio/mpeg" />
+            </audio>
+          )}
+          <Space size="large" style={{ marginTop: "20px" }} />
+          <div className="finishedResults-buttons">
+            <button
+              className="finishedResults-button-1"
+              onClick={handleRecordClick}
+            >
+              Generate another prompt
+            </button>
+            <button
+              className="finishedResults-button-2"
+              onClick={handleHomeClick}
+            >
+              Return to app
+            </button>
+          </div>
 
-                    {/* <div>
+          {/* <div>
                         <h3>{typeof(recordingData.transcript)}</h3>
                         <h3>{parseDisfluencies(recordingData.transcript).stringSplit}</h3>
                         <h3>{redHighlight(recordingData.transcript)}</h3>
                     </div> */}
-                </div>
-            )
-        } else {
-            return (
-                <div className="loadingResults">
-                    <h1 className="question-prompt">Congrats! You did it!</h1>
-                    <Space size="middle">
-                        <Spin size="large" />
-                    </Space>
-                </div>
-            )
-        }
-    }
-    
-    return (
-        <div className="finishedPage">
-            {renderResults()}
         </div>
-    );
-}
+      );
+    } else {
+      return (
+        <div className="loadingResults">
+          <h1 className="question-prompt">Congrats! You did it!</h1>
+          <Space size="middle">
+            <Spin size="large" />
+          </Space>
+        </div>
+      );
+    }
+  };
+
+  return <div className="finishedPage">{renderResults()}</div>;
+};
 
 export default Finished;
